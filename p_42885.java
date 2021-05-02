b5f1153d58ed90c6325cabf5a35bbd68a9bb0a8c
Eunsooyoun <afsd0729@naver.com>
2021-05-02 18:30:18 +0900

    lv2_구명보트 시간초과 해결

--- p_42885.java
+++ p_42885.java
@@ -6,28 +6,56 @@ public class p_42885 {
 
     public static void main(String[] args) {
         // 프로그래머스 lv2_구명보트
-        int[] people = { 70, 50, 80, 50 };
+        int[] people = { 70, 50, 80 };
         int limit = 100;
 
         System.out.println(solution(people, limit));
     }
 
     public static int solution(int[] people, int limit) {
+        // 시간초과 해결 (list에서 remove 안하는 방식으로 변경)
         int answer = 0;
         Arrays.sort(people);
         List<Integer> list = new ArrayList<Integer>();
         for (int i = people.length - 1; i >= 0; i--) {
             list.add(people[i]);
         }
-        while (list.size() > 0) {
-            if (list.size() > 1 && list.get(0) + list.get(list.size() - 1) <= limit) {
-                list.remove(list.size() - 1);
+
+        int j = list.size() - 1;
+        int i = 0;
+        while (i <= j) {
+            if (i == j) {
+                answer++;
+                i++;
+                j--;
+            } else {
+                if (list.get(i) + list.get(j) <= limit) {
+                    j--;
+                }
+                i++;
+                answer++;
             }
-            answer++;
-            list.remove(0);
         }
 
         return answer;
     }
+
+    // public static int solution(int[] people, int limit) {
+    // int answer = 0;
+    // Arrays.sort(people);
+    // List<Integer> list = new ArrayList<Integer>();
+    // for (int i = people.length - 1; i >= 0; i--) {
+    // list.add(people[i]);
+    // }
+    // while (list.size() > 0) {
+    // if (list.size() > 1 && list.get(0) + list.get(list.size() - 1) <= limit) {
+    // list.remove(list.size() - 1);
+    // }
+    // answer++;
+    // list.remove(0);
+    // }
+
+    // return answer;
+    // }
 
 }
